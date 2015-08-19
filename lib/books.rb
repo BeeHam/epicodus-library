f49ebf88756493580917cc089b722297ef910835
Tony Navarro & Brigham Thompson <student@epicodus.com>
2015-08-19 11:06:59 -0700

    writes save spec, and == spec and passes

--- lib/books.rb
+++ lib/books.rb
@@ -13,12 +13,23 @@ class Book
     books = []
     returned_books.each() do |book|
       id = book.fetch('id').to_i()
-      book_id = book.fetch('book_id')
+      book_id = book.fetch('book_id').to_i()
       title = book.fetch('title')
       author = book.fetch('author')
       pub_date = book.fetch('pub_date')
       books.push(Book.new({:id => id, :title => title, :pub_date => pub_date, :author => author, :book_id => book_id}))
     end
     books
+  end
+
+  define_method(:save) do
+    if ! @id
+      @id = "NULL"
+    end
+    result = DB.exec("INSERT INTO books (title, pub_date, author, book_id) VALUES ('#{@title}', '#{@pub_date}', '#{@author}', #{@book_id}) RETURNING id;")
+    @id = result.first().fetch('id').to_i()
+  end
+  define_method(:==) do |another_book|
+    self.title.==(another_book.title).&(self.book_id.==(another_book.book_id)).&(self.id.==(another_book.id)).&(self.pub_date.==(another_book.pub_date)).&(self.author.==(another_book.author))
   end
 end
--- spec/books_spec.rb
+++ spec/books_spec.rb
@@ -3,7 +3,22 @@ require('spec_helper')
 describe(Book) do
   describe('.all') do
     it('shows an empty array of books') do
-    expect(Book.all).to(eq([]))
+    expect(Book.all).to eq []
+    end
+  end
+
+  describe('#save') do
+    it('adds books to the array of saved books') do
+      test_book = Book.new({:title => "Cat in the Hat", :author => "Dr. Seuss", :pub_date => "1942-01-01", :book_id => 1})
+      test_book.save()
+      expect(Book.all()).to(eq([test_book]))
+    end
+  end
+  describe('#==') do
+    it('is the same task if it is the same description') do
+      test_book1 = Book.new({:id => nil, :title => "Cat in the Hat", :author => "Dr. Seuss", :pub_date => "1942-01-01", :book_id => 1})
+      test_book2 = Book.new({:id => nil, :title => "Cat in the Hat", :author => "Dr. Seuss", :pub_date => "1942-01-01", :book_id => 1})
+      expect(test_book1).to eq test_book2
     end
   end
 end
